6817c60c8477d0cc4733937117850c97dc6d3a63
Robert <33263090+Box-Bop@users.noreply.github.com>
2018-11-28 20:18:41 +0200

    Initial commit

---
+++ Notes/DatabaseService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace Notes
+{
+    class DatabaseService
+    {
+        SQLiteConnection db;
+
+        public void CreateDatabase()
+        {
+            string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "mydatabase.db3");
+            db = new SQLiteConnection(dbPath);
+            db.CreateTable<Note>();
+        }
+
+        public void CreateTableWithData()
+        {
+            db.CreateTable<Note>();
+            if (db.Table<Note>().Count() == 0)
+            {
+                var newPost = new Note();
+                newPost.NoteTitle = "SQLite notes";
+                newPost.NoteContent = "Make an SQLite note taking app";
+                newPost.PostTime = Convert.ToString(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+                db.Insert(newPost);
+            }
+        }
+
+        public void AddPost(string title, string content)
+        {
+            var newPost = new Note();
+            newPost.NoteTitle = "SQLite notes";
+            newPost.NoteContent = "Make an SQLite note taking app";
+            newPost.PostTime = Convert.ToString(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+            db.Insert(newPost);
+        }
+
+        public void DeletePost(long id)
+        {
+            db.Delete<Note>(id);
+        }
+
+        public TableQuery<Note> GetAllPosts()
+        {
+            var table = db.Table<Note>();
+            return table;
+        }
+    }
+}
---
+++ Notes/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using static Android.Widget.AdapterView;
+
+namespace Notes
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            var postListView = FindViewById<ListView>(Resource.Id.listView1);
+            var addPostTitleEditText = FindViewById<EditText>(Resource.Id.editText1);
+            var addPostContentEditText = FindViewById<EditText>(Resource.Id.editText2);
+            var addPostButton = FindViewById<Button>(Resource.Id.button1);
+
+            var databaseService = new DatabaseService();
+            databaseService.CreateDatabase();
+            databaseService.CreateTableWithData();
+            var posts = databaseService.GetAllPosts();
+
+            postListView.Adapter = new CustomAdapter(this, posts.ToList());
+
+            postListView.ItemClick += (object sender, ItemClickEventArgs e) =>
+            {
+                var stockID = posts.ToList()[e.Position];
+                //var itemToDelete = stockListView.GetItemAtPosition(e.Position);
+                databaseService.DeletePost(stockID.Id);
+
+                posts = databaseService.GetAllPosts();
+                postListView.Adapter = new CustomAdapter(this, posts.ToList());
+            };
+
+            addPostButton.Click += delegate
+            {
+                var postTitle = addPostTitleEditText.Text;
+                var postContent = addPostContentEditText.Text;
+                databaseService.AddPost(postTitle, postContent);
+
+                posts = databaseService.GetAllPosts();
+                postListView.Adapter = new CustomAdapter(this, posts.ToList());
+
+                addPostTitleEditText.Text = "";
+            };
+        }
+    }
+}
